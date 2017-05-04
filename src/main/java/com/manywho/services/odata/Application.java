e681d87724458d40e9a21a9fc138e60c158030fa
Jonjo McKay <jonjo@jonjomckay.com>
2017-05-04 13:45:34 -0400

    Added support for running on Tomcat

--- src/main/java/com/manywho/services/odata/Application.java
+++ src/main/java/com/manywho/services/odata/Application.java
@@ -1,11 +1,24 @@
 package com.manywho.services.odata;
 
 import com.manywho.sdk.services.servers.EmbeddedServer;
+import com.manywho.sdk.services.servers.Servlet3Server;
 import com.manywho.sdk.services.servers.undertow.UndertowServer;
 import com.manywho.services.odata.odata4j.ODataApacheHttpConsumer;
 import org.odata4j.consumer.ODataConsumers;
 
-public class Application {
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class Application extends Servlet3Server {
+    public Application() {
+        // We need to override the consumer with our own custom one that doesn't try and load CXF
+        System.setProperty(ODataConsumers.CONSUMERIMPL_PROPERTY, ODataApacheHttpConsumer.class.getCanonicalName());
+
+        this.addModule(new ApplicationModule());
+        this.setApplication(Application.class);
+        this.start();
+    }
+
     public static void main(String[] args) throws Exception {
         // We need to override the consumer with our own custom one that doesn't try and load CXF
         System.setProperty(ODataConsumers.CONSUMERIMPL_PROPERTY, ODataApacheHttpConsumer.class.getCanonicalName());
