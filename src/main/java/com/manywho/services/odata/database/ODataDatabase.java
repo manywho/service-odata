4999b689d04e7d79672b4758eabd6e7bcca685d6
Jonjo McKay <jonjo@jonjomckay.com>
2020-04-19 18:15:43 +0100

    FLOW-1564: Added stuff required for Kubernetes

--- src/main/java/com/manywho/services/odata/database/ODataDatabase.java
+++ src/main/java/com/manywho/services/odata/database/ODataDatabase.java
@@ -1,5 +1,6 @@
 package com.manywho.services.odata.database;
 
+import com.manywho.sdk.api.draw.content.Command;
 import com.manywho.sdk.api.run.elements.type.ListFilter;
 import com.manywho.sdk.api.run.elements.type.MObject;
 import com.manywho.sdk.api.run.elements.type.ObjectDataType;
@@ -18,42 +19,42 @@ public class ODataDatabase implements RawDatabase<ApplicationConfiguration> {
     }
 
     @Override
-    public MObject create(ApplicationConfiguration configuration, MObject mObject) {
-        throw new RuntimeException("Creating records using this service is not supported");
-    }
-
-    @Override
-    public List<MObject> create(ApplicationConfiguration configuration, List<MObject> list) {
-        throw new RuntimeException("Creating records using this service is not supported");
-    }
-
-    @Override
-    public void delete(ApplicationConfiguration configuration, MObject mObject) {
-        throw new RuntimeException("Deleting records using this service is not supported");
-    }
-
-    @Override
-    public void delete(ApplicationConfiguration configuration, List<MObject> list) {
-        throw new RuntimeException("Deleting records using this service is not supported");
-    }
-
-    @Override
-    public MObject update(ApplicationConfiguration configuration, MObject mObject) {
-        throw new RuntimeException("Updating records using this service is not supported");
-    }
-
-    @Override
-    public List<MObject> update(ApplicationConfiguration configuration, List<MObject> list) {
-        throw new RuntimeException("Updating records using this service is not supported");
-    }
-
-    @Override
-    public MObject find(ApplicationConfiguration configuration, ObjectDataType objectDataType, String id) {
+    public MObject find(ApplicationConfiguration configuration, ObjectDataType objectDataType, Command command, String id) {
         return manager.find(configuration, objectDataType, id);
     }
 
     @Override
-    public List<MObject> findAll(ApplicationConfiguration configuration, ObjectDataType objectDataType, ListFilter filter) {
+    public List<MObject> findAll(ApplicationConfiguration configuration, ObjectDataType objectDataType, Command command, ListFilter filter, List<MObject> objects) {
         return manager.findAll(configuration, objectDataType, filter);
+    }
+
+    @Override
+    public MObject create(ApplicationConfiguration configuration, ObjectDataType objectDataType, MObject object) {
+        throw new RuntimeException("Creating records using this service is not supported");
+    }
+
+    @Override
+    public List<MObject> create(ApplicationConfiguration configuration, ObjectDataType objectDataType, List<MObject> objects) {
+        throw new RuntimeException("Creating records using this service is not supported");
+    }
+
+    @Override
+    public void delete(ApplicationConfiguration configuration, ObjectDataType objectDataType, MObject object) {
+        throw new RuntimeException("Deleting records using this service is not supported");
+    }
+
+    @Override
+    public void delete(ApplicationConfiguration configuration, ObjectDataType objectDataType, List<MObject> objects) {
+        throw new RuntimeException("Deleting records using this service is not supported");
+    }
+
+    @Override
+    public MObject update(ApplicationConfiguration configuration, ObjectDataType objectDataType, MObject object) {
+        throw new RuntimeException("Updating records using this service is not supported");
+    }
+
+    @Override
+    public List<MObject> update(ApplicationConfiguration configuration, ObjectDataType objectDataType, List<MObject> objects) {
+        throw new RuntimeException("Updating records using this service is not supported");
     }
 }
